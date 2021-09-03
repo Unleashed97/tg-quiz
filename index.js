076346385c4606c625ca58e8600c89a10a1e91f7
Nichkov Roman <nichkovroman@gmail.com>
2021-09-03 10:24:38 +0300

    i18n custom, ru, en

--- index.js
+++ index.js
@@ -2,218 +2,59 @@ process.env.NTBA_FIX_319 = 1
 
 require('dotenv').config()
 const TelegramBot = require('node-telegram-bot-api')
-
 const bot = new TelegramBot(process.env.TOKEN, { polling: true })
 
-const menu = {
-    start: {
-        title: 'Hello, here you can find some quizzes',
-        keyboard: {
-            reply_markup: {
-                keyboard: [['Quizzes'], ['Settings']],
-            },
-        },
-        quizzes: {
-            title: 'Choose one of the following quizzes',
-            keyboard: {
-                reply_markup: {
-                    keyboard: [
-                        ['European capitals', 'Asian capitals'],
-                        ['American capitals', 'Oceania capitals'],
-                        ['back'],
-                    ],
-                },
-            },
-        },
-        settings: {
-            title: 'Here you can change the language, etc',
-            keyboard: {
-                reply_markup: {
-                    keyboard: [['Language'], ['back']],
-                },
-            },
-        },
-    },
-}
+const en = require('./locales/en.json')
+const ru = require('./locales/ru.json')
+
+let i18n = en
 
 // commands
 const COMMAND_START = 'start'
 const COMMAND_HELP = 'help'
+const COMMAND_LANGUAGE = 'language'
+
+bot.setMyCommands([
+    { command: 'start', description: `${i18n.command.start}` },
+    { command: 'help', description: 'about this bot' },
+    { command: 'language', description: `${i18n.command.languge}` },
+])
 
 bot.onText(new RegExp(`/(.*)`), (msg, [source, match]) => {
     const { chat, text } = msg
+    if (msg.from.language_code === 'en') {
+        i18n = en
+    } else if (msg.from.language_code === 'ru') {
+        i18n = ru
+    }
+
     switch (match) {
         case COMMAND_START:
-            bot.sendMessage(chat.id, `${menu.start.title}`, menu.start.keyboard)
+            bot.sendMessage(chat.id, `${i18n.menu.start.title}`, {
+                reply_markup: {
+                    keyboard: i18n.menu.start.keyboard,
+                },
+            })
             break
         case COMMAND_HELP:
-            bot.sendMessage(chat.id, 'help', {
+            bot.sendMessage(chat.id, i18n.command.help, {
                 reply_markup: {
                     remove_keyboard: true,
                 },
             })
             break
+        case COMMAND_LANGUAGE:
+            bot.sendMessage(chat.id, i18n.menu.start.languages.title, {
+                reply_markup: {
+                    keyboard: i18n.menu.start.languages.keyboard,
+                },
+            })
+            break
         default:
-            bot.sendMessage(
-                chat.id,
-                'I dont know this command, please try again or see /help',
-            )
+            bot.sendMessage(chat.id, i18n.command.other)
             break
     }
 })
-
-const countries = {
-    europe: {
-        Albania: 'Tirana',
-        Andorra: 'Andorra la Vella',
-        Austria: 'Vienna',
-        Belarus: 'Minsk',
-        Belgium: 'Brussels',
-        'Bosnia and Herzegovina': 'Sarajevo',
-        Bulgaria: 'Sofia',
-        Croatia: 'Zagreb',
-        Cyprus: 'Nicosia',
-        'Czech Republic': 'Prague',
-        Denmark: 'Copenhagen',
-        Estonia: 'Tallinn',
-        Finland: 'Helsinki',
-        France: 'Paris',
-        Germany: 'Berlin',
-        Greece: 'Athens',
-        Hungary: 'Budapest',
-        Iceland: 'Reykjavík',
-        Ireland: 'Dublin',
-        Italy: 'Rome',
-        Latvia: 'Riga',
-        Liechtenstein: 'Vaduz',
-        Lithuania: 'Vilnius',
-        Luxembourg: 'Luxembourg',
-        Macedonia: 'Skopje',
-        Malta: 'Valletta',
-        Moldova: 'Chișinău',
-        Monaco: 'Monaco',
-        Montenegro: 'Podgorica',
-        Netherlands: 'Amsterdam',
-        Norway: 'Oslo',
-        Poland: 'Warsaw',
-        Portugal: 'Lisbon',
-        Kosovo: 'Pristina',
-        Romania: 'Bucharest',
-        Russia: 'Moscow',
-        'San Marino': 'City of San Marino',
-        Serbia: 'Belgrade',
-        Slovakia: 'Bratislava',
-        Slovenia: 'Ljubljana',
-        Spain: 'Madrid',
-        Sweden: 'Stockholm',
-        Switzerland: 'Bern',
-        Ukraine: 'Kiev',
-        England: 'London',
-    },
-    asia: {
-        Afghanistan: 'Kabul',
-        Armenia: 'Yerevan',
-        Azerbaijan: 'Baku',
-        Bahrain: 'Manama',
-        Bangladesh: 'Dhaka',
-        Bhutan: 'Thimphu',
-        Brunei: 'Bandar Seri Begawan',
-        Cambodia: 'Phnom Penh',
-        China: 'Beijing',
-        Georgia: 'Tbilisi',
-        India: 'New Delhi',
-        Indonesia: 'Jakarta',
-        Iran: 'Tehran',
-        Iraq: 'Baghdad',
-        Israel: 'Jerusalem',
-        Japan: 'Tokyo',
-        Jordan: 'Amman',
-        Kazakhstan: 'Astana',
-        Kuwait: 'Kuwait City',
-        Kyrgyzstan: 'Bishkek',
-        "Lao People's Democratic Republic": 'Vientiane',
-        Lebanon: 'Beirut',
-        Malaysia: 'Kuala Lumpur',
-        Maldives: 'Malé',
-        Mongolia: 'Ulan Bator',
-        Myanmar: 'Naypyidaw',
-        Nepal: 'Kathmandu',
-        'North Korea': 'Pyongyang',
-        Oman: 'Muscat',
-        Pakistan: 'Islamabad',
-        Palestine: 'Ramallah',
-        Philippines: 'Manila',
-        Qatar: 'Doha',
-        'Saudi Arabia': 'Riyadh',
-        Singapore: 'Singapore',
-        'South Korea': 'Seoul',
-        'Sri Lanka': 'Colombo',
-        'Syrian Arab Republic': 'Damascus',
-        Taiwan: 'Taipei',
-        Tajikistan: 'Dushanbe',
-        Thailand: 'Bangkok',
-        'Timor-Leste': 'Dili',
-        Turkey: 'Ankara',
-        Turkmenistan: 'Ashgabat',
-        'United Arab Emirates': 'Abu Dhabi',
-        Uzbekistan: 'Tashkent',
-        'Viet Nam': 'Hanoi',
-        Yemen: "Sana'a",
-    },
-    america: {
-        'Antigua and Barbuda': "Saint John's",
-        Argentina: 'Buenos Aires',
-        Bahamas: 'Nassau',
-        Barbados: 'Bridgetown',
-        Belize: 'Belmopan',
-        Bolivia: 'Sucre',
-        Brazil: 'Brasília',
-        Canada: 'Ottawa',
-        'Cayman Islands': 'George Town',
-        Chile: 'Santiago',
-        Colombia: 'Bogotá',
-        'Costa Rica': 'San José',
-        Cuba: 'Havana',
-        Dominica: 'Roseau',
-        'Dominican Republic': 'Santo Domingo',
-        Ecuador: 'Quito',
-        'El Salvador': 'San Salvador',
-        Grenada: "St. George's",
-        Guatemala: 'Guatemala City',
-        Guyana: 'Georgetown',
-        Haiti: 'Port-au-Prince',
-        Honduras: 'Tegucigalpa',
-        Jamaica: 'Kingston',
-        Mexico: 'Mexico City',
-        Nicaragua: 'Managua',
-        Panama: 'Panama City',
-        Paraguay: 'Asunción',
-        Peru: 'Lima',
-        'Puerto Rico': 'San Juan',
-        'Saint Kitts and Nevis': 'Basseterre',
-        'Saint Lucia': 'Castries',
-        'Saint Vincent and the Grenadines': 'Kingstown',
-        Suriname: 'Paramaribo',
-        'Trinidad and Tobago': 'Port of Spain',
-        'United States of America': 'Washington, D.C.',
-        Uruguay: 'Montevideo',
-        Venezuela: 'Caracas',
-    },
-    oceania: {
-        Australia: 'Canberra',
-        Fiji: 'Suva',
-        Kiribati: 'South Tarawa',
-        'Marshall Islands': 'Majuro',
-        Nauru: 'Yaren',
-        'New Zealand': 'Wellington',
-        'Papua New Guinea': 'Port Moresby',
-        Samoa: 'Apia',
-        'Solomon Islands': 'Honiara',
-        Tonga: "Nuku'alofa",
-        Tuvalu: 'Funafuti',
-        Vanuatu: 'Port Vila',
-    },
-}
 
 const getRandomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min)) + min
@@ -250,22 +91,28 @@ const quizGenerator = (region) => {
         for (let i = 0; i < 3; i++) {
             let randomNumber = getRandomNumber(
                 0,
-                Object.values(countries[region]).length,
+                Object.values(i18n.countries[region]).length,
             )
-            if (Object.values(countries[region])[randomNumber] === capital) {
+            if (
+                Object.values(i18n.countries[region])[randomNumber] === capital
+            ) {
                 randomNumber = getRandomNumber(
                     0,
-                    Object.values(countries[region]).length,
+                    Object.values(i18n.countries[region]).length,
                 )
-                answers.push(Object.values(countries[region])[randomNumber])
+                answers.push(
+                    Object.values(i18n.countries[region])[randomNumber],
+                )
             } else {
-                answers.push(Object.values(countries[region])[randomNumber])
+                answers.push(
+                    Object.values(i18n.countries[region])[randomNumber],
+                )
             }
         }
         return shuffleArray(answers)
     }
 
-    shuffleArray(Object.entries(countries[region])).map(([key, value]) => {
+    shuffleArray(Object.entries(i18n.countries[region])).map(([key, value]) => {
         quizData.push(
             new QuizQuestion(key, value, answersGeneration(value), null),
         )
@@ -276,14 +123,14 @@ const getScore = (chatId, text) => {
     if (text === quizData[questionNumber].capital) {
         quizData[questionNumber].isRight = true
         ++score
-        bot.sendMessage(chatId, `<b>Correct!</b>`, {
+        bot.sendMessage(chatId, `<b>${i18n.quiz.answerCorrect}</b>`, {
             parse_mode: 'HTML',
         })
     } else {
         quizData[questionNumber].isRight = false
         message = bot.sendMessage(
             chatId,
-            `<b>Wrong!</b> The correct answer is <b>${quizData[questionNumber].capital}</b>`,
+            `<b>${i18n.quiz.answerWrong}</b> ${i18n.quiz.correctAnswer} <b>${quizData[questionNumber].capital}</b>`,
             {
                 parse_mode: 'HTML',
             },
@@ -306,7 +153,7 @@ const quiz = (chatId, region) => {
                 quizData[questionNumber].options[2],
                 quizData[questionNumber].options[3],
             ],
-            ['exit'],
+            [`${i18n.quiz.exit}`],
         ]
 
         buttons = [
@@ -319,9 +166,9 @@ const quiz = (chatId, region) => {
         let number = questionNumber
         bot.sendMessage(
             chatId,
-            `<b>${++number}</b>/${
-                Object.keys(countries[region]).length
-            } What's the capital of ${quizData[questionNumber].country}`,
+            `<b>${++number}</b>/${Object.keys(i18n.countries[region]).length} ${
+                i18n.quiz.question
+            } ${quizData[questionNumber].country}`,
             {
                 reply_markup: {
                     keyboard: keyboard,
@@ -332,10 +179,10 @@ const quiz = (chatId, region) => {
     } else {
         bot.sendMessage(
             chatId,
-            `You've scored ${score} out of ${
-                Object.keys(countries[region]).length
+            `${i18n.quiz.score} ${score}/${
+                Object.keys(i18n.countries[region]).length
             }`,
-            menu.start.keyboard,
+            i18n.menu.start.keyboard,
         )
         questionNumber = 0
         quizData.length = 0
@@ -346,33 +193,56 @@ const quiz = (chatId, region) => {
 bot.on('message', (msg) => {
     const { chat, text, message_id } = msg
     switch (text) {
-        case 'Quizzes':
-            bot.sendMessage(
-                chat.id,
-                `${menu.start.quizzes.title}`,
-                menu.start.quizzes.keyboard,
-            )
+        case `${i18n.buttons.quizzes}`:
+            bot.sendMessage(chat.id, `${i18n.menu.start.quizzes.title}`, {
+                reply_markup: {
+                    keyboard: i18n.menu.start.quizzes.keyboard,
+                },
+            })
             break
-        case 'Settings':
-            bot.sendMessage(
-                chat.id,
-                `${menu.start.settings.title}`,
-                menu.start.settings.keyboard,
-            )
+        case `${i18n.buttons.settings}`:
+            bot.sendMessage(chat.id, `${i18n.menu.start.settings.title}`, {
+                reply_markup: {
+                    keyboard: i18n.menu.start.settings.keyboard,
+                },
+            })
             break
-        case 'European capitals':
+        case `${i18n.buttons.language}`:
+            bot.sendMessage(chat.id, `${i18n.menu.start.languages.title}`, {
+                reply_markup: {
+                    keyboard: i18n.menu.start.languages.keyboard,
+                },
+            })
+            break
+        case `${i18n.buttons.english}`:
+            i18n = en
+            bot.sendMessage(chat.id, `${i18n.menu.start.title}`, {
+                reply_markup: {
+                    keyboard: i18n.menu.start.keyboard,
+                },
+            })
+            break
+        case `${i18n.buttons.russian}`:
+            i18n = ru
+            bot.sendMessage(chat.id, `${i18n.menu.start.title}`, {
+                reply_markup: {
+                    keyboard: i18n.menu.start.keyboard,
+                },
+            })
+            break
+        case `${i18n.buttons.europeCapitals}`:
             region = 'europe'
             quiz(chat.id, region)
             break
-        case 'Asian capitals':
+        case `${i18n.buttons.asianCapitals}`:
             region = 'asia'
             quiz(chat.id, region)
             break
-        case 'American capitals':
+        case `${i18n.buttons.americanCapitals}`:
             region = 'america'
             quiz(chat.id, region)
             break
-        case 'Oceania capitals':
+        case `${i18n.buttons.oceaniaCapitals}`:
             region = 'oceania'
             quiz(chat.id, region)
             break
@@ -386,13 +256,17 @@ bot.on('message', (msg) => {
                 quiz(chat.id, region)
             }, 100)
             break
-        case 'exit':
+        case `${i18n.buttons.exit}`:
             questionNumber = 0
             region = ''
             quizData.length = 0
             score = 0
-        case 'back':
-            bot.sendMessage(chat.id, `${menu.start.title}`, menu.start.keyboard)
+        case `${i18n.buttons.back}`:
+            bot.sendMessage(chat.id, `${i18n.menu.start.title}`, {
+                reply_markup: {
+                    keyboard: i18n.menu.start.keyboard,
+                },
+            })
             break
     }
 })
