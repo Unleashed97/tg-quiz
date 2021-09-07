8e03c1c763428de61a6cbb623149e3899dfd0b8d
Nichkov Roman <nichkovroman@gmail.com>
2021-09-07 11:53:52 +0300

    url express

--- web.js
+++ web.js
@@ -4,10 +4,6 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/', function (req, res) {
-    res.json({ version: packageInfo.version })
-})
-
 app.listen(process.env.PORT, () => {
     console.log(`Listening on port ${process.env.PORT}`)
 })
