2b31c2638df62d2532877d449b0080a07b2be5ce
Nichkov Roman <nichkovroman@gmail.com>
2021-09-07 11:49:21 +0300

    heroku deploy fix

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-process.env.NTBA_FIX_319 = 1
-
 require('dotenv').config()
 
 let bot = require('./bot')
--- web.js
+++ web.js
@@ -4,6 +4,10 @@ const app = express()
 
 app.use(express.json())
 
+app.get('/', function (req, res) {
+    res.json({ version: packageInfo.version })
+})
+
 app.listen(process.env.PORT, () => {
     console.log(`Listening on port ${process.env.PORT}`)
 })
